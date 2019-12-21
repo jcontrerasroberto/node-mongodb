193a2e58f4a25662382caa2e104a99df1b5db767
José Roberto Contreras Barrita <contreras.jrobert@gmail.com>
2019-12-20 23:01:10 -0600

    Terminado

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const passport = require('passport');
 
 
 router.get('/users/signup', (req, res) => {
@@ -9,5 +11,47 @@ router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 });
 
+router.post('/users/signin', passport.authenticate('local', {
+	successRedirect: '/notes',
+	failureRedirect: '/users/signin',
+	failureFlash: true
+}));
+
+router.post('/users/signup', async (req, res) => {
+	const { name, email, password, confirm_password} = req.body;
+	const errors= [];
+	if (name == '') {
+		errors.push({text: 'Please insert a name'});
+	}
+	if (email == '') {
+		errors.push({text: 'Please insert a email'});
+	}
+	if (password != confirm_password) {
+		errors.push({text: 'Passwords do not match'});
+	}
+	if (password.length < 4) {
+		errors.push({text: 'Password must be at least 4 characters'});
+	}
+	if (errors.length > 0) {
+		res.render('users/signup', {errors, name, email});
+	}else{
+		const emailUser = await User.findOne({email: email});
+		if (emailUser) {
+			req.flash('error_msg', 'Email is already in use');
+			res.redirect('/users/signup');
+		}else{
+			const newUser = new User({name, email, password});
+			newUser.password = await newUser.encryptPassword(password);
+			await newUser.save();
+			req.flash('success_msg', 'Estas registrado');
+			res.redirect('/users/signin');	
+		}
+	}
+});
+
+router.get('/users/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
 
 module.exports = router;
